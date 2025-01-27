68381f3025f7269545b9f2b4ce2d238569325b8f
Memorix101 <Memorix101@users.noreply.github.com>
2025-01-27 05:21:27 +0100

    Update sdl2_gamepad_to_dinput.h

--- src/harness/platforms/sdl2_gamepad_to_dinput.h
+++ src/harness/platforms/sdl2_gamepad_to_dinput.h
@@ -10,24 +10,24 @@ typedef struct {
 // Define the mappings
 static GamepadMapping sdlGamepadToDirectInputKeyNum = {
     .buttonMapping = {
-        [SDL_CONTROLLER_BUTTON_A] = 0x48,  // KP_8 (Accelerate - fallback)
+        [SDL_CONTROLLER_BUTTON_A] = 0x1C,  // KP_8 (Accelerate - fallback)
         [SDL_CONTROLLER_BUTTON_B] = 0x4C,  // KP_5 (Handbrake)
-        [SDL_CONTROLLER_BUTTON_X] = 0x01,  // SPACEBAR (Action)
-        [SDL_CONTROLLER_BUTTON_Y] = 0x1C,  // 'C' (Cockpit Toggle)
-        [SDL_CONTROLLER_BUTTON_START] = 0x39, // ESCAPE (Pause/Menu)
+        [SDL_CONTROLLER_BUTTON_X] = 0xD2,  // SPACEBAR (Action)
+        [SDL_CONTROLLER_BUTTON_Y] = 0x0E,  // 'C' (Cockpit Toggle)
+        [SDL_CONTROLLER_BUTTON_START] = 0x01, // ESCAPE (Pause/Menu)
         [SDL_CONTROLLER_BUTTON_DPAD_UP] = 0x48, // UP
-        [SDL_CONTROLLER_BUTTON_DPAD_DOWN] = 0xD0, // DOWN
+        [SDL_CONTROLLER_BUTTON_DPAD_DOWN] = 0x50, // DOWN
         [SDL_CONTROLLER_BUTTON_DPAD_LEFT] = 0x4B, // LEFT
         [SDL_CONTROLLER_BUTTON_DPAD_RIGHT] = 0x4D  // RIGHT
     },
     .axisPositive = {
         [SDL_CONTROLLER_AXIS_TRIGGERRIGHT] = 0x48,  // KP_8 (Accelerate)
-        [SDL_CONTROLLER_AXIS_TRIGGERLEFT]  = 0x4C,  // KP_2 (Brake)
-        [SDL_CONTROLLER_AXIS_LEFTX] = 0x12, // 'E' (Look Right)
+        [SDL_CONTROLLER_AXIS_TRIGGERLEFT]  = 0x50,  // KP_2 (Brake)
+        [SDL_CONTROLLER_AXIS_LEFTX] = 0x4B, // 'E' (Look Right)
         [SDL_CONTROLLER_AXIS_LEFTY] = 0x2E  // 'C' (Cockpit Toggle)
     },
     .axisNegative = {
-        [SDL_CONTROLLER_AXIS_LEFTX] = 0x10, // 'Q' (Look Left)
-        [SDL_CONTROLLER_AXIS_LEFTY] = 0x11  // 'W' (Look Forward)
+        [SDL_CONTROLLER_AXIS_LEFTX] = 0x0F, // 'Q' (Look Left)
+        [SDL_CONTROLLER_AXIS_LEFTY] = 0x48  // 'W' (Look Forward)
     }
 };
