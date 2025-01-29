0fb98af09fc3f938fbb039f7d22d6c08a7dfd405
Memorix101 <Memorix101@users.noreply.github.com>
2025-01-29 06:12:09 +0100

    Revert "Merge branch 'GPF:dreamcast' into dreamcast"
    This reverts commit 1dbd531042b0e01240900b14c718c071bf02ce43, reversing
    changes made to 1937c389c61933be23ea3d18d183b61b2728a4b6.

--- make_cdi.sh
+++ make_cdi.sh
@@ -1,2 +1,2 @@
- ./tools/mkdcdisc -e dethrace.elf -d DETHRACE -d MUSIC -d DATA -o dethrace-dc.cdi -n "dethrace" -N
+ ./tools/mkdcdisc -e dethrace.elf -d DETHRACE -o dethrace-dc.cdi -n "dethrace" -N
 ~/flycast-x86_64.AppImage dethrace-dc.cdi
